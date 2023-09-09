24f8ad55f377e3ad05e30285960562e8d78fbbd1
Gustakx36 <gustavobruno12333@gmail.com>
2023-09-09 19:21:01 -0300

    Modificacoes v2.5

--- api_hamburgueria/hamburgueria_users/entities/orders.py
+++ api_hamburgueria/hamburgueria_users/entities/orders.py
@@ -50,6 +50,7 @@ class Order:
             order = order_items.Order_items()
             for item in result:
                 order.insertSimples(item)
+            self.listOptionsUpdateIgnore.pop(3)
             self.updateSimples(params.dict(), pedido)
         return {
             'response' : resultOrder,
