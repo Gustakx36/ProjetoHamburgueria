7ca169ddf8d720f1697164ce543969990ecfb712
GKabum <gustavo.bruno@kabum.com.br>
2023-10-16 20:19:20 -0300

    Modificações v4.2

--- api_hamburgueria/hamburgueria_users/entities/buildResponse/buildResponse.py
+++ api_hamburgueria/hamburgueria_users/entities/buildResponse/buildResponse.py
@@ -5,9 +5,7 @@ def selectSimplesResponse(Objeto):
     try:
         result = Objeto.selectSimples()
         if result['response'] and len(result['object']) != 0:
-            return JsonResponse({
-                'object' : result['object']
-            }, status=200, json_dumps_params={'ensure_ascii': False})
+            return JsonResponse(result['object'], status=200, json_dumps_params={'ensure_ascii': False})
         return JsonResponse({'response' : result['text']}, status=204, json_dumps_params={'ensure_ascii': False})
     except:
         return JsonResponse({'response' : 'Erro ao conectar com o banco!'}, status=500, json_dumps_params={'ensure_ascii': False})
@@ -16,9 +14,7 @@ def selectSimplesPorIdResponse(Objeto, id):
     try:
         result = Objeto.selectSimplesPorId(id)
         if result['response']:
-            return JsonResponse({
-                'object' : result['object']
-            }, status=200, json_dumps_params={'ensure_ascii': False})
+            return JsonResponse(result['object'], status=200, json_dumps_params={'ensure_ascii': False})
         return JsonResponse({'response' : result['text']}, status=204, json_dumps_params={'ensure_ascii': False})
     except:
         return JsonResponse({'response' : 'Erro ao conectar com o banco!'}, status=500, json_dumps_params={'ensure_ascii': False})
@@ -62,9 +58,7 @@ def selectSimplesOrderItemResponse(Objeto, id_pedido):
     try:
         result = Objeto.selectSimples(id_pedido)
         if result['response']:
-            return JsonResponse({
-                'object' : result['object']
-            }, status=200, json_dumps_params={'ensure_ascii': False})
+            return JsonResponse(result['object'], status=200, json_dumps_params={'ensure_ascii': False})
         return JsonResponse({'response' : result['text']}, status=204, json_dumps_params={'ensure_ascii': False})
     except:
         return JsonResponse({'response' : 'Erro ao conectar com o banco!'}, status=500, json_dumps_params={'ensure_ascii': False})
